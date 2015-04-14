584beb37bb5353aa6485dcf3447b38daafcfd982
Dario Gonzalez <jdariogonzalez@gmail.com>
2015-04-14 00:02:42 -0300

    se modifico el mensaj

--- src/main/java/com/pulsaractivo/controller/HomeController.java
+++ src/main/java/com/pulsaractivo/controller/HomeController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hola, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Mundo") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
