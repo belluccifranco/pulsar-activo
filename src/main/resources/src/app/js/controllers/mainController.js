b510a90ff38b94d502fbb3a304883c1bc1060a0e
Facundo Sosa López <fssl2004@gmail.com>
2015-04-21 23:33:43 -0300

    - Mejorada la interfaz de main.

--- src/main/resources/src/app/js/controllers/mainController.js
+++ src/main/resources/src/app/js/controllers/mainController.js
@@ -2,7 +2,7 @@
     'use strict';
     angular.module('pulsarActivoManager')
         .controller('mainController', function($scope, uiGmapGoogleMapApi) {
-        	$scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
+        	$scope.map = {center: {latitude: -27.4856987, longitude: -58.8023838 }, zoom: 13 };
         	uiGmapGoogleMapApi.then(function(maps) {
 
     		});
