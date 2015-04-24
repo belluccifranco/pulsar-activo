7e662db93c9f0326189d90da2f7c02a6b4e6f039
Facundo Sosa López <fssl2004@gmail.com>
2015-04-24 01:54:09 -0300

    - Agregados datos de prueba para el listado de dispositivos en la pantalla principal.

--- src/main/resources/src/app/js/controllers/mainController.js
+++ src/main/resources/src/app/js/controllers/mainController.js
@@ -1,7 +1,69 @@
 (function(){
     'use strict';
+
+    var devices = {
+            1: {
+                id: 1,
+                name: 'Caminante 01'
+            },
+            2: {
+                id: 2,
+                name: 'Caminante 05'
+            },
+            3: {
+                id: 3,
+                name: 'Caminante 08'
+            },
+            4: {
+                id: 4,
+                name: 'Caminante 09'
+            },
+            5: {
+                id: 5,
+                name: 'Caminante 10'
+            },
+            6: {
+                id: 6,
+                name: 'EMI 01'
+            },
+            7: {
+                id: 7,
+                name: 'EMI 04'
+            },
+            8: {
+                id: 8,
+                name: 'EMI 08'
+            }
+        },
+        fleets = {
+            1: {
+                id: 1,
+                name: 'Caminantes Resistencia',
+                devices: [devices[1], devices[2], devices[3], devices[4], devices[5]]
+            },
+            2: {
+                id: 2,
+                name: 'EMI',
+                devices: [devices[6], devices[7], devices[8]]
+            }
+        },
+        groups = {
+            1: {
+                id: 1,
+                name: '911 Policía del Chaco',
+                fleets: [fleets[1]]
+            },
+            2: {
+                id: 2,
+                name: 'Municipalidad de Resistencia',
+                fleets: [fleets[2]]
+            }
+        };
+
+
     angular.module('pulsarActivoManager')
         .controller('mainController', function($scope, uiGmapGoogleMapApi) {
+            $scope.groups = groups;
         	$scope.map = {center: {latitude: -27.4856987, longitude: -58.8023838 }, zoom: 13 };
         	uiGmapGoogleMapApi.then(function(maps) {
 
