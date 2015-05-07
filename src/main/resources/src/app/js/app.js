7991115c5ab4a8d6f1b622b1e66642a37f4921e5
Franco Bellucci <belluccifranco@gmail.com>
2015-05-07 01:04:35 -0300

    Format segun las practicas recomendadas por la doc de angularjs.

--- src/main/resources/src/app/js/app.js
+++ src/main/resources/src/app/js/app.js
@@ -1,12 +1,12 @@
 (function () {
     'use strict';
 
-    angular.module('pulsarActivo', ['ngRoute', 'ngResource', 'ngCookies', 'uiGmapgoogle-maps', /*'btford.socket-io'*/])
+    angular.module('pulsarActivo', ['ngRoute', 'ngResource', 'ngCookies', 'uiGmapgoogle-maps' /*,'btford.socket-io'*/])
         /*.factory('socket', function (socketFactory) {
-            return socketFactory({
-                ioSocket: io.connect('http://localhost:3000')
-            });
-        })*/
+         return socketFactory({
+         ioSocket: io.connect('http://localhost:3000')
+         });
+         })*/
         .config(['uiGmapGoogleMapApiProvider', function (uiGmapGoogleMapApiProvider) {
             uiGmapGoogleMapApiProvider.configure({
                 //key: 'your api key',
@@ -24,5 +24,23 @@
                     controller: 'LoginController'
                 });
             $locationProvider.html5Mode(false).hashPrefix('!');
+        }])
+        .config(['$httpProvider', function ($httpProvider) {
+
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRF-TOKEN';
+            $httpProvider.defaults.xsrfCookieName = 'CSRF-TOKEN';
+
+            function b(a) {
+                return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e16] + 1e16).replace(/[01]/g, b)
+            }
+            $httpProvider.interceptors.push(function () {
+                return {
+                    'request': function (config) {
+                        // put a new random secret into our CSRF-TOKEN Cookie after each response
+                        document.cookie = 'CSRF-TOKEN=' + b();
+                        return config;
+                    }
+                };
+            });
         }]);
 }());
