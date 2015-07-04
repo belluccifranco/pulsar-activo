561154d085a0dbe6fa7ccece7d496f858d323255
Facundo Sosa López <fssl2004@gmail.com>
2015-07-04 00:09:28 -0300

    - Eliminado el archivo MenuController.js, solo se usaba para indicar la opción seleccionada en el menú. Eso se solucionó usando la directiva ui-sref-active="active" (ui-router).

--- src/main/resources/src/app/js/controllers/MenuController.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('pulsarActivo')
-        .controller('MenuController', ['$scope', '$location', function ($scope, $location) {
-            $scope.isActive = function (viewLocation) {
-                return $location.path().indexOf(viewLocation) === 0;
-            };
-        }]);
-}());
